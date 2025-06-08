d146fd0c8662ae443a2ac27ef350a6476f6ebffb
lmrco <43510+lmrco@users.noreply.github.com>
2025-06-08 14:49:09 +0200

    Haskell Project Scaffold with CI/CD, Tests, Linting, and Docs (#1)

---
+++ src/MyLib.hs
@@ -0,0 +1,9 @@
+{- |
+Module      : MyLib
+Description : Simple math utilities for learning Haskell
+-}
+module MyLib (sumTwo) where
+
+-- | Adds two integers together
+sumTwo :: Int -> Int -> Int
+sumTwo x y = x + y
